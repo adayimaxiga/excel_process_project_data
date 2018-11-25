4ad952f00b537299b4129d17302abaa2b6901d3b
adayimaxiga <adayimaxiga@hotmail.com>
2018-11-25 11:31:51 +0800

    final

--- ExcelProcessMain.py
+++ ExcelProcessMain.py
@@ -4,12 +4,9 @@
 import xlrd
 import xlwt
 import ngender
-#import re
-
-import sys
 
 file_name_output=input("请从输入期刊名，出版编辑类（中国出版，中国科技期刊研究，出版发行研究，出版科学，现代出版，科技与出版，编辑之友）新闻与传媒类（国际新闻界，当代传播，新闻与传播研究，新闻大学，新闻界，新闻记者，现代传播）")
-
+shuangyiliu ={'北京大学','中国人民大学','清华大学','北京航空航天大学','北京理工大学','中国农业大学','北京师范大学','中央民族大学','南开大学','天津大学','大连理工大学','吉林大学','哈尔滨工业大学','复旦大学','同济大学','上海交通大学','华东师范大学','南京大学','东南大学','浙江大学','中国科学技术大学','厦门大学','山东大学','中国海洋大学','武汉大学','华中科技大学','中南大学','中山大学','华南理工大学','四川大学','重庆大学','电子科技大学','西安交通大学','西北工业大学','兰州大学','国防科技大学','东北大学','郑州大学','湖南大学','云南大学','西北农林科技大学','新疆大学'}
 all_chubanlei = {'中国出版','中国科技期刊研究','出版发行研究','出版科学','现代出版','科技与出版','编辑之友'}
 chubanlei = False
 
@@ -92,7 +89,10 @@ Sheet_pipei.write(0, 18, '权值_基金')  # A1
 Sheet_pipei.write(0, 19, '第一责任人')  # A1
 Sheet_pipei.write(0, 20, '性别估计')  # A1
 Sheet_pipei.write(0, 21, '性别估计概率')  # A1
-
+Sheet_pipei.write(0, 22, '性别权值')  # A1
+Sheet_pipei.write(0, 23, '发表月份权值')  # A1
+Sheet_pipei.write(0, 24, '摘要题目权重')  # A1
+Sheet_pipei.write(0, 25, '作者单位权重')  # A1
 #存储所有导出的类
 data_daochu=[]
 #存储宝宝要的类
@@ -473,18 +473,58 @@ if __name__ == "__main__":
                     Sheet_pipei.write(count + 1, 13,data_daochu_deleteuseless[j].Fund)  # A1
                     Sheet_pipei.write(count + 1, 14,data_daochu_deleteuseless[j].PageCount)  # A1
                     Sheet_pipei.write(count + 1, 19,data_daochu_deleteuseless[j].FirstDuty)  # A1
+                    #print(data_daochu_deleteuseless[j].PubTime)
+                    yuefen_start = data_daochu_deleteuseless[j].PubTime.find('-')
+                    yuefen_n = safe_int(data_daochu_deleteuseless[j].PubTime[yuefen_start+1:yuefen_start+3])
+                    if(yuefen_n<4):
+                        Sheet_pipei.write(count + 1, 23, '0')
+                    elif(yuefen_n<7):
+                        Sheet_pipei.write(count + 1, 23, '1')
+                    elif (yuefen_n < 10):
+                        Sheet_pipei.write(count + 1, 23, '2')
+                    else:
+                        Sheet_pipei.write(count + 1, 23, '3')
+                    #print ( yuefen_n )
 
+                    flag_title_have_dingliang = data_daochu_deleteuseless[j].Title.find('定量')
+                    flag_title_have_shizheng = data_daochu_deleteuseless[j].Title.find('实证')
+                    flag_summary_have_dingliang = data_daochu_deleteuseless[j].Summary.find('定量')
+                    flag_summary_have_shizheng = data_daochu_deleteuseless[j].Summary.find('实证')
+
+                    if((flag_title_have_dingliang!=-1)or(flag_title_have_shizheng!=-1)or(flag_summary_have_dingliang!=-1)or(flag_summary_have_shizheng!=-1)):
+                        Sheet_pipei.write(count + 1, 24, '1')
+                    else:
+                        Sheet_pipei.write(count + 1, 24, '0')
+
+                    flag_shuangyiliu=0
+                    if((data_daochu_deleteuseless[j].Organ.find('编辑部')!=-1)or(data_daochu_deleteuseless[j].Organ.find('杂志社')!=-1)):
+                        Sheet_pipei.write(count + 1, 25, '0')
+                    elif((data_daochu_deleteuseless[j].Organ.find('学会')!=-1)or(data_daochu_deleteuseless[j].Organ.find('协会')!=-1)):
+                        Sheet_pipei.write(count + 1, 25, '1')
+                    elif ((data_daochu_deleteuseless[j].Organ.find('集团')!=-1) or (data_daochu_deleteuseless[j].Organ.find('公司')!=-1)):
+                        Sheet_pipei.write(count + 1, 25, '2')
+                    elif ((data_daochu_deleteuseless[j].Organ.find('部')!=-1) or (data_daochu_deleteuseless[j].Organ.find('署')!=-1)or (data_daochu_deleteuseless[j].Organ.find('国家')!=-1)):
+                        Sheet_pipei.write(count + 1, 25, '3')
+                    else:
+                        for items in shuangyiliu:
+                            if(data_daochu_deleteuseless[j].Organ.find(items)!=-1):
+                                flag_shuangyiliu =1
+                        if(flag_shuangyiliu==1):
+                            Sheet_pipei.write(count + 1, 25, '4')
+                        else:
+                            Sheet_pipei.write(count + 1, 25, '5')
 
                     if(check_contain_chinese(data_daochu_deleteuseless[j].FirstDuty)):
                         gender_this=ngender.guess(data_daochu_deleteuseless[j].FirstDuty)
 
-
                         if(gender_this[0] == 'male'):
                             Sheet_pipei.write(count + 1, 20, '男')
                             Sheet_pipei.write(count + 1, 21,gender_this[1])
+                            Sheet_pipei.write(count + 1, 22, '1')
                         elif(gender_this[0] == 'female'):
                             Sheet_pipei.write(count + 1, 20, '女')
                             Sheet_pipei.write(count + 1, 21, gender_this[1])
+                            Sheet_pipei.write(count + 1, 22, '0')
 
                     Author_temp = data_beiyin[i].Author.rstrip(';')
                     count_fen=0
@@ -512,361 +552,3 @@ if __name__ == "__main__":
 
 book.save(file_name_output+r'傻敷敷看过来.xls')
 
-
-
-
-
-
-
-
-
-'''
-SheetName2 = Workdata2.sheet_names()
-print(SheetName2)  # 打印名称
-table2 = Workdata2.sheets()[0]
-nrows2 = table2.nrows  # 行数
-ncols2 = table2.ncols  # 列数
-
-SheetName = Workdata.sheet_names()
-print(SheetName)  # 打印名称
-table = Workdata.sheets()[0]
-nrows = table.nrows  # 行数
-ncols = table.ncols  # 列数
-
-# 写入
-book = xlwt.Workbook(encoding='utf-8', style_compression=0)
-SheetElectric = book.add_sheet('表白我家傻敷敷', cell_overwrite_ok=True)
-SheetElectric.write(0, 0, '作者')  # A1
-SheetElectric.write(0, 1, '作者单位')  # AD
-SheetElectric.write(0, 2, '标题')  # T1
-SheetElectric.write(0, 3, '期刊')  # T1
-SheetElectric.write(0, 4, '时间')  # T1
-SheetElectric.write(0, 5, '文章数')  # T1
-SheetElectric.write(0, 6, '按照姓名单位匹配相同作者')  # T1
-ElecCount = 0
-
-SheetMechnical = book.add_sheet('学猫叫', cell_overwrite_ok=True)
-SheetMechnical.write(0, 0, '作者')  # A1
-SheetMechnical.write(0, 1, '作者单位')  # AD
-SheetMechnical.write(0, 2, '标题')  # T1
-SheetMechnical.write(0, 3, '期刊')  # T1
-SheetMechnical.write(0, 4, '时间')  # T1
-SheetMechnical.write(0, 5, '文章数')  # T1
-SheetMechnical.write(0, 6, '按照姓名匹配相同作者')  # T1
-
-Sheet3 = book.add_sheet('喵喵喵喵喵', cell_overwrite_ok=True)
-Sheet3.write(0, 0, '作者')  # A1
-Sheet3.write(0, 1, '作者单位')  # AD
-Sheet3.write(0, 2, '文章数')  # T1
-Sheet3.write(0, 3, '作者及频次')  # T1
-
-Sheet4 = book.add_sheet('傻敷敷不是智慧可爱', cell_overwrite_ok=True)
-Sheet4.write(0, 0, '作者')  # A1
-Sheet4.write(0, 1, '期刊')  # AD
-Sheet4.write(0, 2, '被引频次')  # T1
-
-Sheet5 = book.add_sheet('傻敷敷就是傻敷敷', cell_overwrite_ok=True)
-Sheet5.write(0, 0, '作者')  # A1
-Sheet5.write(0, 1, '作者单位')  # AD
-Sheet5.write(0, 2, '文章数')  # T1
-Sheet5.write(0, 3, '被引频次')  # T1
-
-
-MechCount = 0
-
-
-def ProcessExcelRow(Rawlist):
-    Processedlist = ['header']
-    Flag = False
-    for i in range(len(Rawlist)):
-        if (Rawlist[i] != ''):
-            Processedlist.append(Rawlist[i])
-        #            Processedlist.remove('header')
-    return Processedlist
-
-
-# 声明一个类
-class MessegeOfRobomasterPerson:
-    def __init__(self, Name, Office, title, Book, Date):
-        self.name = Name
-        self.office = Office
-        self.title = title
-        self.book = Book
-        self.date = Date
-        self.count = 0
-        self.frequence =0
-    def countSet(self, Count):
-        self.count = Count
-    def frequenceSet(self, frequence):
-        self.frequence = frequence
-    def TimeSet(self, Time):
-        self.time = round(Time, 1)
-
-    def print(self):
-        print("作者：" + str(self.name))
-        print("作者单位：" + str(self.office))
-        print("标题：" + str(self.title))
-        print("期刊：" + str(self.title))
-
-    def ReturnDate(self):
-        return self.date
-    def ReturnFrequence(self):
-        return self.frequence
-    def ReturnBook(self):
-        return self.book
-
-    def ReturnName(self):
-        return self.name
-
-    def ReturnOffice(self):
-        return self.office
-
-    def Returntitle(self):
-        return self.title
-
-    def Returncount(self):
-        return self.count
-
-    def __cmp__(self, other):
-        if self.name != other.name:
-            return self.name < other.name
-        elif self.name == other.name:
-            return self.office < other.name
-
-    def __lt__(self, other):
-        if self.name != other.name:
-            return self.name < other.name
-        elif self.name == other.name:
-            if self.office != other.office:
-                return self.office < other.office
-            elif self.office == other.office:
-                return self.title < other.title
-
-    def __eq__(self, other):
-        if (self.name == other.name) and (self.office == other.office) and (self.title == other.title):
-            return True
-        else:
-            return False
-
-
-def safe_int(num):
-    try:
-        return int(num)
-    except ValueError:
-        result = []
-        for c in num:
-            if not ('0' <= c <= '9'):
-                break
-            result.append(c)
-        if len(result) == 0:
-            return 0
-        return int(''.join(result))
-
-
-def ConvertToHours(StringTime):
-    BufferTimeHour = StringTime[0:2]
-    BufferTimeMinute = StringTime[3:5]
-    BufferTimeHourInt = safe_int(BufferTimeHour)
-    BufferTimeMinuteInt = safe_int(BufferTimeMinute)
-    return BufferTimeHourInt + BufferTimeMinuteInt / 60
-
-
-def isNum(value):
-    try:
-        value + 1
-    except TypeError:
-        return False
-    else:
-        return True
-
-
-person = []
-person2 = []
-if __name__ == "__main__":
-    # 全局保存
-    Count = 0
-    FlagStartRecord = False
-    namename = ""
-    officeoffice = ""
-    titletitle = ""
-    bookbook = ""
-    datedate = ""
-
-    grid = [['.', '.', '.', '.', '.', '.'],
-            ['.', 'O', 'O', '.', '.', '.'],
-            ['O', 'O', 'O', 'O', '.', '.'],
-            ['O', 'O', 'O', 'O', 'O', '.'],
-            ['.', 'O', 'O', 'O', 'O', 'O'],
-            ['O', 'O', 'O', 'O', 'O', '.'],
-            ['O', 'O', 'O', 'O', '.', '.'],
-            ['.', 'O', 'O', '.', '.', '.'],
-            ['.', '.', '.', '.', '.', '.']]
-    for i in range(6):
-        for x in range(10):
-            if x == 9:
-                print('\r')
-            else:
-                print(grid[x][i],end=' ')
-
-    tempnum = int(input("傻敷敷告诉我一个数字"))
-    for i in range(nrows):
-        # 循环初始变量
-        FlagNameUpdate = False
-
-        # 获取原始行列
-        ListTempDeleteTemp = ProcessExcelRow(table.row_values(i))
-        if (len(ListTempDeleteTemp) > 2):
-            if (ListTempDeleteTemp[1] == "A1"):
-                if (Count > 0):
-                    person.append(MessegeOfRobomasterPerson(namename, officeoffice, titletitle, bookbook, datedate))
-                    namename = ""
-                    officeoffice = ""
-                    titletitle = ""
-                    bookbook = ""
-                    datedate = ""
-                    FlagNameUpdate = True
-                namename = ListTempDeleteTemp[2]
-                Count = Count + 1
-            if (ListTempDeleteTemp[1] == "AD"):
-                officeoffice = ListTempDeleteTemp[2]
-            if (ListTempDeleteTemp[1] == "T1"):
-                titletitle = ListTempDeleteTemp[2]
-            if (ListTempDeleteTemp[1] == "JF"):
-                bookbook = ListTempDeleteTemp[2]
-            if (ListTempDeleteTemp[1] == "YR"):
-                datedate = ListTempDeleteTemp[2]
-    person.append(MessegeOfRobomasterPerson(namename, officeoffice, titletitle, bookbook, datedate))
-    print(Count)
-    person.sort()
-    flagNew = True
-    temp = person[0]
-    i = 1
-    while i < len(person):  # 删除重复部分
-        if (person[i] == temp):
-            person.pop(i)
-        else:
-            temp = person[i]
-            i += 1
-    nametemp = person[0].name
-    officetemp = person[0].office
-    count = 1
-    number = 0
-    for i in range(1, len(person)):
-        if (person[i].name == nametemp) and (officetemp == person[i].office):
-            count += 1
-        else:
-            nametemp = person[i].name
-            officetemp = person[i].office
-            number = count
-            while (count >= 1):
-                person[i - count].countSet(number)
-                count -= 1
-            count = 1
-    person[i].countSet(count)
-
-    for i in range(len(person)):
-        SheetElectric.write(i + 1, 0, person[i].ReturnName())
-        SheetElectric.write(i + 1, 1, person[i].ReturnOffice())
-        SheetElectric.write(i + 1, 2, person[i].Returntitle())
-        SheetElectric.write(i + 1, 3, person[i].ReturnBook())
-        SheetElectric.write(i + 1, 4, person[i].ReturnDate())
-        if (person[i].Returncount() != 0):
-            SheetElectric.write(i + 1, 5, person[i].Returncount())
-    # sheet2处理
-    i=0
-    while i < len(person):  # 删除无office数据
-        if (person[i].ReturnOffice() == ""):
-            person.pop(i)
-        else:
-            person[i].countSet(0)
-            i+=1
-    nametemp = person[0].name
-    count =1
-    number = 0
-    for i in range(1, len(person)):
-        if (person[i].name == nametemp):
-            count += 1
-        else:
-            nametemp = person[i].name
-            number = count
-            while (count >= 1):
-                person[i - count].countSet(number)
-                count -= 1
-            count = 1
-    person[i].countSet(count)
-    for i in range(len(person)):
-        SheetMechnical.write(i + 1, 0, person[i].ReturnName())
-        SheetMechnical.write(i + 1, 1, person[i].ReturnOffice())
-        SheetMechnical.write(i + 1, 2, person[i].Returntitle())
-        SheetMechnical.write(i + 1, 3, person[i].ReturnBook())
-        SheetMechnical.write(i + 1, 4, person[i].ReturnDate())
-        if (person[i].Returncount() != 0):
-            SheetMechnical.write(i + 1, 5, person[i].Returncount())
-
-    #sheet3
-    i = 1
-    tempnamename = person[0].ReturnName()
-    while i < len(person):  # 删除重复部分
-        if (person[i].ReturnName() == tempnamename):
-            person.pop(i)
-        else:
-            tempnamename = person[i].ReturnName()
-            i += 1
-
-    for i in range(len(person)):
-        Sheet3.write(i + 1, 0, person[i].ReturnName())
-        Sheet3.write(i + 1, 1, person[i].ReturnOffice())
-        Sheet3.write(i + 1, 2, person[i].Returncount())
-    #读取excel2数据
-    namename2 = ""
-    bookbook2 = ""
-    countcount2 =0
-
-    for i in range(1,nrows2):
-        # 获取原始行列
-        ListTempDeleteTemp2 = ProcessExcelRow(table2.row_values(i))
-
-        if (len(ListTempDeleteTemp2) > 2):
-            namename2 = ListTempDeleteTemp2[2]
-            bookbook2 = ListTempDeleteTemp2[3]
-            countcount2 = ListTempDeleteTemp2[4]
-        if(len(ListTempDeleteTemp2)==5):
-            countcount2 =0
-        if(countcount2<tempnum):
-            countcount2 =0
-        person2.append(MessegeOfRobomasterPerson(namename2,"","",bookbook2,""))
-        person2[i-1].countSet(countcount2)
-    #print(person2[0].ReturnName())
-    person2.sort()
-    #print(person2[0].ReturnName())
-    #sheet4
-    temp22=person2[0].ReturnName()
-    i=1
-    while i < len(person2):  #删除重复部分
-        if (person2[i].ReturnName() == temp22):
-            person2[i-1].countSet(person2[i-1].Returncount()+person2[i].Returncount())
-            person2.pop(i)
-        else:
-            temp22 = person2[i].ReturnName()
-            i += 1
-    for i in range(len(person2)):
-        Sheet4.write(i + 1, 0, person2[i].ReturnName())
-        Sheet4.write(i + 1, 1, person2[i].ReturnBook())
-        Sheet4.write(i + 1, 2, person2[i].Returncount())
-
-    for i in range(len(person)):
-        for j in range(len(person2)):
-            if(person[i].ReturnName()==person2[j].ReturnName()):
-                person[i].frequenceSet(person2[j].Returncount())
-    j=0
-    for i in range(len(person)):
-        if(person[i].ReturnFrequence()!=0):
-            Sheet5.write(j + 1, 0, person[i].ReturnName())
-            Sheet5.write(j + 1, 1, person[i].ReturnOffice())
-            Sheet5.write(j + 1, 2, person[i].Returncount())
-            Sheet5.write(j + 1, 3, person[i].ReturnFrequence())
-            Sheet5.write(j + 1, 4, person[i].ReturnBook())
-            j+=1
-
-book.save(r'傻敷敷看过来.xls')
-'''
